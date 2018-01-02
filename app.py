2ff3be9dba3265c333db58f407e411569a949a38
joe brower <zyoosun@gmail.com>
2018-01-02 13:35:29 -0600

    hello

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "cranston"
+
+@app.route("/bazaar", methods=["GET"])
+def bazaar_export_orders():
+    if request.args['action'] == "export":
+        username = request.args['SS-UserName']
+        password = request.args['SS-Password']
+        action = request.args['action']
+        startDate = request.args['start_date']
+        endDate = request.args['end_date']
+        page = request.args['page']
+
+        if username == 'mango' and password == "papaya":
+            return "Hi Mango", 200
+        else:
+            return "You are no Mango!", 401
+    else:
+        return "Me no understand", 400
+
+@app.route("/bazaar", methods=["POST"])
+def bazaar_import_orders():
+    pass
+
+app.run(port=5000, debug=True)
