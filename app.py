b9b114c56e480cc32c6ec117b6dd2cda84bc7698
joe brower <zyoosun@gmail.com>
2018-04-12 11:37:06 -0500

    log rendering

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 from flask import Flask, request, send_file, jsonify
-from fakeDB import xmlSample
+from fakeDB import sampleOrder, sampleOrderUpdate
 import logging
 
 logging.basicConfig(filename="logfile.log", level=logging.INFO, format="%(levelname)s:%(asctime)s: %(message)s")
 
 app = Flask(__name__)
 
-# @app.before_request
-# def before_request():
-#     if True:
-#         print("HEADERS", request.headers)
-#         print("REQ_path", request.path)
-#         print("ARGS",request.args)
-#         print("DATA",request.data)
-#         print("FORM",request.form)
 
 @app.route("/")
 def home():
@@ -37,24 +29,53 @@ def bazaar_export_orders():
     if username != 'mango' or password != 'papaya':
         return jsonify({"message": "Authorization failed."}), 401
 
-    return xmlSample, 200
+    return sampleOrder, 200
         
 
 
 @app.route("/bazaar", methods=["POST"])
-def bazaar_import_orders():
-    logging.info("Connecting from: " + request.remote_addr)
-    logging.info("Arguments included: " + str(request.url))
-    logging.info("Headers included: " + str(request.headers))
+def bazaar_ship_notify():
+    logging.info("Notification from: " + request.remote_addr)
+    logging.info("Arguements included: " + str(request.args))
     logging.info(str(request.data))
-    return "Hello!"
 
+    try:
+        username = request.args['SS-UserName']
+        password = request.args['SS-Password']
+        action = request.args['action']
+        orderNumber = request.args['order_number']
+        carrier = request.args['carrier']
+        trackingNumber = request.args['tracking_number']
+    except Exception:
+        return jsonify({"message": "Expected Parameters not received."}), 400
+
+    if username != 'mango' or password != 'papaya':
+        return jsonify({"message": "Authorization failed."}), 401
+
+    if request.args['service']:
+        carrierService = request.args['service']
+    else:
+        carrierService = "marked_as_shipped"
+
+    sampleOrderUpdate = str(request.data)
+
+    return jsonify({"message": "Shipment notification received."}), 200
+
+"""Endpoint on site from which log files can be downloaded"""
 @app.route("/postlogs")
 def view_post_logs():
-    return send_file("logfile.log")
+    html_response = ""
+
+    with open("logfile.log") as readlogs:
+        for line in readlogs:
+            html_response += "<p>{}</p>".format(line)
+
+    return html_response
+
 
 if __name__ == '__main__':
     app.run()
 
-# app.run(port=5000)
+"""Runs app as a local Flask server for testing"""
+# app.run(port=5000, debug=True)
 
