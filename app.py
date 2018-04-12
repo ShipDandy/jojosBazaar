f710a05fa5eacbab17d7faf6193d542b3e172a22
joe brower <zyoosun@gmail.com>
2018-04-12 13:33:55 -0500

    heroku testing 4

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def view_post_logs():
     #         html_response += "<p>{}</p>".format(line)
 
     # return html_response
-    return send_file("requirements.txt")
+    return send_file("logfile.log", as_attachment=True)
 
 
 if __name__ == '__main__':
