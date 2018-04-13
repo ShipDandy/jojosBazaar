cc0ebdfa61bf038003d4d00f904c04fc54a9838e
joe brower <zyoosun@gmail.com>
2018-04-13 12:20:00 -0500

    added comments on functionality, updated python version

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, request, send_file, jsonify
-from fakeDB import sampleOrder, sampleOrderUpdate
+from fakeDB import sampleOrder, sampleOrderUpdate # in place of a database we will use this python file with a pre-composed order.
 import logging
 
+"""When developing custom endpoints having logging capture incoming information from ShipStation in invaluable."""
 logging.basicConfig(filename="logfile.log", level=logging.INFO, format="%(levelname)s:%(asctime)s: %(message)s")
 
 app = Flask(__name__)
 
-
+"""This endpoint represents the public-facing side of your system if there is one."""
 @app.route("/")
 def home():
     return "Welcome to your website!"
 
+"""An idempotent endpoint set up to communicate order information with ShipStation. GET requests for orders created or modified between the startDate and endDate parameters sent as part of a string query sent from ShipStation."""
 @app.route("/bazaar", methods=["GET"])
 def bazaar_export_orders():
     logging.info("Import request from: " + request.remote_addr)
     logging.info("Arguments included: " + str(request.args))
 
+    # extract values passed in the string parameters of the GET request from ShipStation.
     try:
         username = request.args['SS-UserName']
         password = request.args['SS-Password']
@@ -24,15 +27,17 @@ def bazaar_export_orders():
         endDate = request.args['end_date']
         page = request.args['page']
     except Exception:
-        return jsonify({"message": "Expected Parameters not received."}), 400
+        return jsonify({"message": "Expected Parameters not received."}), 400 # useful check that all parameters are being sent when mocking your own calls for testing purposes.
 
+    # simple authorization method, production authorization will probably be much fancier.
     if username != 'mango' or password != 'papaya':
         return jsonify({"message": "Authorization failed."}), 401
 
+    # After successful authorization you would normally take the startDate and endDate and query your database for new/updated orders to pass back as XML. If you needed to send you orders over multiple pages you would also take advantage of the page value passed along. For this example we simply contact our fakeDB.py file for our response.
     return sampleOrder, 200
         
 
-
+"""An idempotent endpoint set up to receive updated order information from ShipStation. POST requests pass along shipping information for orders including carrier used as well as tracking numbers."""
 @app.route("/bazaar", methods=["POST"])
 def bazaar_ship_notify():
     logging.info("Notification from: " + request.remote_addr)
@@ -52,20 +57,23 @@ def bazaar_ship_notify():
     if username != 'mango' or password != 'papaya':
         return jsonify({"message": "Authorization failed."}), 401
 
+    # The carrier service selected when a label is generated in ShipStation will be passed along, if an order was marked as shipped either in app or via API a value will not be passed in the string parameter.
     if request.args['service']:
         carrierService = request.args['service']
     else:
         carrierService = "marked_as_shipped"
 
+    # In production you would ingest information from the string paramters passed and/or the accompanying XML data passed back to update the order's entry in your database, for this example simply send it back to fakeDB.py.
     sampleOrderUpdate = str(request.data)
 
     return jsonify({"message": "Shipment notification received."}), 200
 
-"""Endpoint on site from which log files can be downloaded"""
+"""Endpoint on site from which log files can be downloaded."""
 @app.route("/getlogs", methods=["GET"])
 def download_logs():    
     return send_file("logfile.log", as_attachment=True, attachment_filename="logfile.log")
 
+"""Provides an in browser view of logs, helpful when hosting on ephmeral filesystems like Heroku."""
 @app.route("/logs", methods=["GET"])
 def view_logs():
     html_response = ""
@@ -76,9 +84,9 @@ def view_logs():
 
     return html_response
 
+
 if __name__ == '__main__':
     app.run()
 
-"""Runs app as a local Flask server for testing"""
+"""Runs app on a local Flask server for testing"""
 # app.run(port=5000, debug=True)
-
